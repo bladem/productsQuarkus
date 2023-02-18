dd051c01fe2668b28f05f5c0f86067ace6a31265
Pau Sanchez <pausanchez@dosatic.com>
2023-02-18 12:13:23 +0100

    Dockerizando la app

--- src/test/java/org/pausanchez/ProductApiTest.java
+++ src/test/java/org/pausanchez/ProductApiTest.java
@@ -1,23 +1,34 @@
 package org.pausanchez;
 
 import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Disabled;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
+import org.pausanchez.entities.Product;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 public class ProductApiTest {
 
     @Test
-    @Disabled
+    public void testAddProduct(){
+        Product product = new Product();
+        product.setDescription("hola");
+        product.setName("pau");
+
+        given().contentType(ContentType.JSON)
+                .body(product)
+                .when().post("/product")
+                .then()
+                .statusCode(200);
+    }
+
+    @Test
     public void testHelloEndpoint() {
         given()
           .when().get("/product")
           .then()
-             .statusCode(200)
-             .body(is("Hello from RESTEasy Reactive"));
+             .statusCode(200);
     }
 
 }
